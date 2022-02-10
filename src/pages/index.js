ac1bd60bc584ce0a15af7687a2f0608330d09189
Calilo Kizan <ckizan23@gmail.com>
2022-02-10 09:22:43 -0300

    some description changes

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,7 @@
-import Acomplishments from '../components/Acomplishments/Acomplishments';
-import BgAnimation from '../components/BackgrooundAnimation/BackgroundAnimation';
-import Hero from '../components/Hero/Hero';
 import Projects from '../components/Projects/Projects';
-import Technologies from '../components/Technologies/Technologies';
-import Timeline from '../components/TimeLine/TimeLine';
 import Educational from '../components/Educational/Educational';
 import Zeca from '../components/Zeca/Zeca';
 import { Layout } from '../layout/Layout';
-import { Section } from '../styles/GlobalComponents';
 import Manu from '../components/Manu/Manu';
 import Petwork from '../components/Petwork/Petwork';
 
